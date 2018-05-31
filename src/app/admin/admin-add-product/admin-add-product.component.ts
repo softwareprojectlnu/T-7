77b4926d603b86ea8710c48f1e96a8de16e69c91
ebtisam23 <ebtisamahmed23@yahoo.com>
2018-05-31 14:55:03 +0200

    Hello world

---
+++ src/app/admin/admin-add-product/admin-add-product.component.ts
@@ -0,0 +1,69 @@
+import {AngularFireStorage} from 'angularfire2/storage';
+import {Product} from './../../models/product';
+import {Router, ActivatedRoute} from '@angular/router';
+import {ProductService} from './../../services/product.service';
+import {category} from './../../models/category';
+import {Observable} from 'rxjs/Observable';
+import {CategoryService} from './../../services/category.service';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import 'rxjs/add/operator/take';
+
+@Component({
+  selector: 'app-admin-add-product',
+  templateUrl: './admin-add-product.component.html',
+  
+  styleUrls: ['./admin-add-product.component.css']
+})
+export class AdminProductFormComponent implements OnInit {
+  key: string;
+  product: Product = <Product>{};
+  categoryKey: string;
+
+  categories: Observable<category[]>;
+
+  constructor(
+    private storage: AngularFireStorage,
+    private productService: ProductService,
+    private router: Router,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute) {
+    this.categories = categoryService.categories;
+  }
+
+  ngOnInit() {
+    const key = this.route.snapshot.paramMap.get('key');
+    if (key) {
+      this.key = key;
+      this.productService.get(key).take(1).subscribe(product => {
+        this.product = product;
+        this.categoryKey = product.category.id;
+      });
+    }
+  }
+
+  save(form: NgForm) {
+    const product = this.product;
+
+    product.category = <any>this.categoryService.getRef(form.value.category).ref;
+    if (this.key) product.key = this.key;
+
+    this.productService.save(product);
+
+    this.router.navigate(['admin/products']);
+  }
+
+  remove() {
+    this.productService.remove(this.key);
+    this.router.navigate(['admin/products']);
+  }
+
+  fileChanged(event) {
+    const file = event.target.files[0];
+
+    const filePath = 'newProducts/' + file.name;
+    const task = this.storage.upload(filePath, file);
+
+    task.downloadURL().take(1).subscribe(url => this.product.imageUrl = url);
+  }
+}
